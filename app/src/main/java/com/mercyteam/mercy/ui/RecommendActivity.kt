af06646e62583770079c6d2ea037259be05af31e
sery270 <sery270@naver.com>
2020-06-07 01:28:20 +0900

    Recommend 관련 레이아웃 작성 중

---
+++ app/src/main/java/com/mercyteam/mercy/ui/RecommendActivity.kt
@@ -0,0 +1,10 @@
+package com.mercyteam.mercy.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecommendActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
