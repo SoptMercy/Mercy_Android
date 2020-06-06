f3dc77212d3172f760a84889a6758d2c1e6eda21
yanghyesun <nbn0809@naver.com>
2020-06-07 03:12:22 +0900

    [hyesun] error solve

--- app/src/main/java/com/mercyteam/mercy/ui/RecommendActivity.kt
+++ app/src/main/java/com/mercyteam/mercy/ui/RecommendActivity.kt
@@ -2,7 +2,6 @@ package com.mercyteam.mercy.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.FragmentManager
 import androidx.viewpager.widget.ViewPager
 import com.mercyteam.mercy.R
 import kotlinx.android.synthetic.main.activity_recommend.*
@@ -11,32 +10,15 @@ class RecommendActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_recommend)
 
-        vp_home.adapter = RecommendAdapter(
-            supportFragmentManager, 2
-        )
-        vp_home.offscreenPageLimit = 2
-        vp_home.addOnPageChangeListener(object : ViewPager.OnPageChangeListener{
-            override fun onPageScrollStateChanged(state: Int) {
+        val recommentAdapter = RecommendAdapter(supportFragmentManager, 3)
+        vp_home.run {
+            adapter = recommentAdapter
+            offscreenPageLimit = 2
+        }
+        tl_home.setupWithViewPager(vp_home)
 
-            }
-
-            override fun onPageScrolled(
-                position: Int,
-                positionOffset: Float,
-                positionOffsetPixels: Int
-            ) {
-
-            }
-
-            override fun onPageSelected(position: Int) {
-                //네비게이션 메뉴 아이템 체크
-                //bottomNavigationView.menu.getItem(position).isChecked = true
-
-            }
-
-        })
 
 //        bottomNavigationView.setOnNavigationItemSelectedListener {
 //            when(it.itemId){
